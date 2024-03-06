87e4678745f53998661a242f4c2427f711f0bc92
Joseg <l20920129@voaxaca.tecnm.mx>
2024-03-06 00:24:09 -0600

    ...

--- controller/Producto.js
+++ controller/Producto.js
@@ -1,51 +1,85 @@
-
+// const db = require('../nexion/db');
 
 class Producto {
-   
-    constructor(){
-        this.productos =[
-            {
-                nombre:"papel",
-                precio:30,
-                codigo:60
-            },
-            {
-                nombre:"jabon",
-                precio:50,
-                codigo:50
-            },
-        ]
-    }
+  constructor() {
+    this.productos = [
+      {
+        codigo: 2253,
+        nombre: "papel",
+        precio: 30,
+        cantidad: 60,
+      },
+      {
+        codigo: 2253,
+        nombre: "jabon",
+        precio: 50,
+        cantidad: 50,
+      },
+    ];
+  }
 
-    agregarProductos(nombre, precio, codigo){
-        const nuevoProducto={
-            nombre:nombre,
-            precio:precio,
-        codigo:codigo}
+  agregarProductos(nombre, precio, codigo, cantidad) {
+    const nuevoProducto = {
+      nombre: nombre,
+      precio: precio,
+      codigo: codigo,
+    };
 
-        this.productos.push(nuevoProducto)
+    this.productos.push(nuevoProducto);
 
-        console.log('Producto agregado:', nuevoProducto);
-    }
+    console.log("Producto agregado:", nuevoProducto);
+  }
 
-    obtenerProductos() {
-        return this.productos;
-    }
-
+  obtenerProductos() {
+    return this.productos;
+  }
 }
 
+//   static obtenerTodos(callback) {
+//     const sql = 'SELECT * FROM Productos';
+//     db.query(sql, callback);
+//   }
 
 
+// console.log('Todos los productos:', productosManager.obtenerProductos());
+
+// const productoActualizado = productosManager.actualizarProducto(1, 'Producto Actualizado', 25.99);
+// console.log('Todos los productos después de la actualización:', productosManager.obtenerProductos());
+
+// productosManager.eliminarProducto(2);
+// console.log('Todos los productos después de la eliminación:', productosManager.obtenerProductos());
 
 
+// producto.js
+// const db = require('./db');
 
+// class Producto {
+//   constructor() {}
 
-   
+//   static obtenerTodos(callback) {
+//     const sql = 'SELECT * FROM Productos';
+//     db.query(sql, callback);
+//   }
 
-    // console.log('Todos los productos:', productosManager.obtenerProductos());
+//   static obtenerPorId(id, callback) {
+//     const sql = 'SELECT * FROM Productos WHERE id = ?';
+//     db.query(sql, [id], callback);
+//   }
 
-    // const productoActualizado = productosManager.actualizarProducto(1, 'Producto Actualizado', 25.99);
-    // console.log('Todos los productos después de la actualización:', productosManager.obtenerProductos());
+//   static agregarProducto(producto, callback) {
+//     const sql = 'INSERT INTO Productos SET ?';
+//     db.query(sql, producto, callback);
+//   }
 
-    // productosManager.eliminarProducto(2);
-    // console.log('Todos los productos después de la eliminación:', productosManager.obtenerProductos());
+//   static actualizarProducto(id, nuevoProducto, callback) {
+//     const sql = 'UPDATE Productos SET ? WHERE id = ?';
+//     db.query(sql, [nuevoProducto, id], callback);
+//   }
+
+//   static eliminarProducto(id, callback) {
+//     const sql = 'DELETE FROM Productos WHERE id = ?';
+//     db.query(sql, [id], callback);
+//   }
+// }
+
+// module.exports = Producto;
--- controller/script.js
+++ controller/script.js
@@ -1,47 +1,49 @@
-
-
-
 const inputBuscar= document.getElementById("inputBuscar");
 const buscarProducto=document.getElementById("buscarProducto")
 
-const productos = new Producto()
+const productos = new Producto();
 const listaVenta =[]
 const nuevoProducto = {} 
+
+
 
 // const btnAgregarProducto= document.getElementById("")
 
 // productos.agregarProductos("jabon",25,2020)
 const obtenerProductos = productos.obtenerProductos()
 
-
-// boton de agregar una nueva compra a mi lista
-
-
 document.getElementById('buscarProducto').addEventListener('click', (e)=>{
   e.preventDefault()
 
   const inputBusca = inputBuscar.value
 
- obtenerProductos.forEach((element)=>{
+  obtenerProductos.forEach((element)=>{
 
-  if(inputBusca ===element.nombre){
+   if(inputBusca ===element.nombre){
 
+    const codigo = element.codigo
     const nombre =element.nombre
     const precio = element.precio
-    agregarfila(nombre, precio)
+    const cantidad = element.cantidad
+    
+    agregarfila(codigo, nombre, precio,cantidad)
   }
  }) 
 
  })
 
-const agregarfila = (nombre, precio)=>{
+const agregarfila = (codigo, nombre, precio,cantidad)=>{
   var tbody = document.querySelector('#tablaProductos tbody')
   const row = tbody.insertRow();
-                columnaNombre = row.insertCell(0);
-                columnaPrecio = row.insertCell(1);         
-    
+                columnaCodigo = row.insertCell(0);
+                columnaNombre = row.insertCell(1);
+                columnaPrecio = row.insertCell(2);  
+                columnaCantidad = row.insertCell(3);
+
+                columnaCodigo.textContent = codigo    
                 columnaNombre.textContent = nombre;
                 columnaPrecio.textContent = precio;
+                columnaCantidad.textContent = cantidad
     
                 document.getElementById('inputBuscar').value = ''          
 
