6223770799a2b6fb65ccf1396d8840498f9a253f
Joseg <l20920129@voaxaca.tecnm.mx>
2024-03-13 17:53:57 -0600

    ..

---
+++ views/products/IMenuJs/Iproducto.js
@@ -0,0 +1,39 @@
+const contenedor= document.getElementById('contenedor');
+
+
+const agregarMenu= (dato)=>{
+
+    contenedor.innerHTML =""
+
+    if(dato ==="nuevo"){
+        contenedor.innerHTML +=`
+        <iframe src="products/menu/nuevo.html" frameborder="0" width="100%" height="85%"></iframe>    
+  
+    `
+    }else if(dato==="modificar"){
+        contenedor.innerHTML +=`
+        <iframe src="products/menu/modificar.html" frameborder="0" width="100%" heiht="85%"></iframe>  `
+    }else if(dato==="eliminar"){
+        contenedor.innerHTML +=`
+        <iframe src="products/menu/eliminar.html" frameborder="0" width="100%" heiht="85%"></iframe>  `
+    }else if(dato==="Departamentos"){
+        contenedor.innerHTML +=`
+        <iframe src="products/menu/departamentos.html" frameborder="0" width="100%" heiht="85%"></iframe>  `
+    }else if(dato==="ventasPeriodo"){
+        contenedor.innerHTML +=`
+        <iframe src="products/menu/ventasPeriodo.html" frameborder="0" width="100%" heiht="85%"></iframe>  `
+    }else if(dato==="promociones"){
+        contenedor.innerHTML +=`
+        <iframe src="products/menu/promociones.html" frameborder="0" width="100%" heiht="85%"></iframe>  `
+    }else if(dato==="importar"){
+        contenedor.innerHTML +=`
+        <iframe src="products/menu/importar.html" frameborder="0" width="100%" heiht="85%"></iframe>  `
+    }else if(dato==="catalogo"){
+        contenedor.innerHTML +=`
+    <iframe src="products/menu/catalogo.html" frameborder="0" width="100%" height="85%"></iframe>    
+    `
+    }
+    
+}
+
+agregarMenu("nuevo");
