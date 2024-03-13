1770e125974417d8a6ba1384aa3dbf180723ece4
Joseg <l20920129@voaxaca.tecnm.mx>
2024-03-12 21:15:43 -0600

    ...

--- views/js/producto.js
+++ views/js/producto.js
@@ -1,1 +1,19 @@
-const contenedor= document.getElementById('contenedor')
+const contenedor= document.getElementById('contenedor');
+
+
+const agregarMenu= (dato)=>{
+
+    contenedor.innerHTML =""
+
+    if(dato ==="nuevo"){
+        contenedor.innerHTML +=`
+
+    <iframe src="menuProductos/nuevo.html" frameborder="0" width="100%" height="100%"></iframe>
+
+    
+    `
+
+    }
+
+    
+}
