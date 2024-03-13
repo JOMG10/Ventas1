22b9d557c14ad0ae1ec36589e8cf75d360a7871d
Joseg <l20920129@voaxaca.tecnm.mx>
2024-03-12 20:20:13 -0600

    ...

--- controller/index.js
+++ controller/index.js
@@ -2,67 +2,63 @@ const btnPaginacion = document.getElementById("buttonPaginacion");
 const contenido = document.getElementById("contenido");
 
 const paginacion = (pagina) => {
+  contenido.innerHTML = "";
 
-    contenido.innerHTML = "";
-  
-    switch (pagina) {
-      case 1:
-        if (pagina === 1) {
-          contenido.innerHTML += `
-                  <iframe src="pagina1.html" frameborder="0" width="100%" height="80%"></iframe>    
-                  `;
-        }
-        break;
-  
-      case 2:
-        if (pagina === 2) {
-          contenido.innerHTML += `
-                      <iframe src="pagina2.html" frameborder="0" width="100%" height="500px"></iframe>    
-                      `;
-        }
-        break;
-  
-      case 3:
-        if (pagina === 3) {
-          contenido.innerHTML += `
-                          <iframe src="pagina3.html" frameborder="0" width="100%" height="500px"></iframe>    
-                          `;
-        }
-        break;
-  
-      case 4:
-        if (pagina === 4) {
-          contenido.innerHTML += `
-                          <iframe src="productos.html" frameborder="0" width="100%" height="100%"></iframe>    
-                          `;
-        }
-        break;
-  
-      case 5:
-        if (pagina === 5) {
-          contenido.innerHTML += `
-                            <iframe src="pagina5.html" frameborder="0" width="100%" height="500px"></iframe>    
-                            `;
-        }
-        break;
-  
-      case 6:
-        if (pagina === 6) {
-          contenido.innerHTML += `
-                                <iframe src="pagina6.html" frameborder="0" width="100%" height="500px"></iframe>    
-                                `;
-        }
-        break;
-  
-      case 7:
-        if (pagina === 7) {
-          contenido.innerHTML += `
-                                    <iframe src="pagina7.html" frameborder="0" width="100%" height="500px"></iframe>    
-                                    `;
-        }
-        break;
-    }
-  };
-  
-  
-  
+  switch (pagina) {
+    case 1:
+      if (pagina === 1) {
+        contenido.innerHTML += `
+          <iframe src="pagina1.html" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+        `;
+      }
+      break;
+
+    case 2:
+      if (pagina === 2) {
+        contenido.innerHTML += `
+          <iframe src="pagina2.html" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+        `;
+      }
+      break;
+
+    case 3:
+      if (pagina === 3) {
+        contenido.innerHTML += `
+          <iframe src="pagina3.html" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+        `;
+      }
+      break;
+
+    case 4:
+      if (pagina === 4) {
+        contenido.innerHTML += `
+          <iframe src="productos.html" frameborder="0" style="width: 100%; height: 80vh;"></iframe>
+        `;
+      }
+      break;
+
+    case 5:
+      if (pagina === 5) {
+        contenido.innerHTML += `
+          <iframe src="pagina5.html" frameborder="0" style="width: 100%; height: 90%;"></iframe>
+        `;
+      }
+      break;
+
+    case 6:
+      if (pagina === 6) {
+        contenido.innerHTML += `
+          <iframe src="pagina6.html" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+        `;
+      }
+      break;
+
+    case 7:
+      if (pagina === 7) {
+        contenido.innerHTML += `
+          <iframe src="pagina7.html" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+        `;
+      }
+      break;
+  }
+};
