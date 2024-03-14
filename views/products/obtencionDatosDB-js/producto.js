b27207f9bdf28482786f64d08ad470a82b1c6605
Joseg <l20920129@voaxaca.tecnm.mx>
2024-03-13 21:08:24 -0600

    ...

--- views/products/obtencionDatosDB-js/producto.js
+++ views/products/obtencionDatosDB-js/producto.js
@@ -10,45 +10,70 @@
     }
     obtenerProductos();
 
-    const mostrarDatos =(data)=>{
-        console.log(data)
-        
+    const mostrarDatos = (data) => {
+        console.log(data);
+    
         const tbody = document.querySelector('#tablaProductos tbody');
         tbody.innerHTML = '';
+    
+        data.forEach((element) => {
+            const nuevaFila = document.createElement("tr");
+    
+            const codigo = element.codigo;
+            const descripcion = element.descripcion;
+            const departamento = element.idDep;
+            const costo = element.costo;
+            const pVenta = element.precioVenta;
+            const pMayoreo = element.precioMayoreo;
+            const existencia = element.existencia;
+            const invMinimo = element.invMinimo;
+            const invMaximo = element.invMaximo;
+            const tipo = element.tipoVenta;
+    
+            nuevaFila.innerHTML = `
+                <td class="columnaM justificado numeroLetra">${codigo}</td>
+                <td class="numeroLetra">${descripcion}</td>
+                <td class="columnaM numeroLetra">${departamento}</td>
+                <td class="columnaP justDerecho numeroLetra">${costo}</td>
+                <td class="columnaP justDerecho numeroLetra">${pVenta}</td>
+                <td class="columnaP justDerecho numeroLetra">${pMayoreo}</td>
+                <td class="columnaP justDerecho numeroLetra">${existencia}</td>
+                <td class="columnaP justDerecho numeroLetra">${invMinimo}</td>
+                <td class="columnaP justDerecho numeroLetra">${invMaximo}</td>
+                <td class="columnaP justDerecho numeroLetra">${tipo}</td>
+            `;
+    
+            tbody.appendChild(nuevaFila);
+        });
+    };
+    
 
-        data.forEach((element, i)=>{
 
-            const codigo = element.codigo
-            const descripcion = element.descripcion
-            const departamento = element.costo
-            const costo = element.costo  
-            const pVenta = element.precioVenta
-            const pMayoreo = element.precioMayoreo
-            const existencia = element.existencia
-            const invMinimo = element.invMinimo
-            const invMaximo = element.invMaximo
-            const tipo = element.tipo   
 
-               const row = tbody.insertRow(i);
-               const cellCodigo = row.insertCell(0)
-               const cellDescripcion = row.insertCell(1);            
-               const cellCosto = row.insertCell(2);
-               const cellPVenta = row.insertCell(3);
-               const cellPMayoreo = row.insertCell(4);
-               const cellExistencia = row.insertCell(5);
-               const cellInvMinimo = row.insertCell(6);
-               const cellInvMaximo = row.insertCell(7);
-               const cellTipo = row.insertCell(8);
+
+            //    const row = tbody.insertRow(i);
+            //    const cellCodigo = row.insertCell(0)
+            //    const cellDescripcion = row.insertCell(1);   
+            //    const cellDepartamento = row.insertCell(2)         
+            //    const cellCosto = row.insertCell(3);
+            //    const cellPVenta = row.insertCell(4);
+            //    const cellPMayoreo = row.insertCell(5);
+            //    const cellExistencia = row.insertCell(6);
+            //    const cellInvMinimo = row.insertCell(7);
+            //    const cellInvMaximo = row.insertCell(8);
+            //    const cellTipo = row.insertCell(9);
               
 
-               cellCodigo.textContent = codigo;       
-               cellDescripcion.textContent = des;
-               cellPrecio.textContent = precio;       
-               cellCantidad.textContent = cantidad
-        })
+            //    cellCodigo.textContent = codigo;       
+            //    cellDescripcion.textContent = descripcion;
+            //    cellDepartamento.textContent = departamento
+            //    cellCosto.textContent = costo;       
+            //    cellPVenta.textContent = pVenta;               
+            //    cellPMayoreo.textContent = pMayoreo;
+            //    cellExistencia.textContent =  existencia;
+            //    cellInvMinimo.textContent = invMinimo;
+            //    cellInvMaximo.textContent = invMaximo;
+            //    cellTipo.textContent = tipo;
 
-
-    }
-
-
-
+               
+           
