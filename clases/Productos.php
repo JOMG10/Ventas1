eb708d648117aca0c2028adf622890475b7ef651
Joseg <l20920129@voaxaca.tecnm.mx>
2024-04-08 16:40:45 -0600

    ....

--- clases/Productos.php
+++ clases/Productos.php
@@ -15,5 +15,26 @@ class Productos extends Conexion {
         return $productos;
     }
 
+    public function obtenerProductoID($id) {
+        $sql = "SELECT * FROM productos WHERE idProducto = $id"; // Debes concatenar el valor de $id en la consulta SQL
+        $result = $this->conexion->query($sql);
+    
+        // Verificar si la consulta fue exitosa
+        if ($result) {
+            // Si hay al menos una fila devuelta, obtén y devuelve los datos
+            if ($result->num_rows > 0) {
+                $datosProducto = $result->fetch_assoc();
+                return $datosProducto;
+            } else {
+                // Si no se encontraron resultados para el ID dado, devuelve un mensaje de error o un arreglo vacío, según sea necesario
+                return ['error' => 'Producto no encontrado'];
+            }
+        } else {
+            // Si hay un error en la consulta SQL, devuelve un mensaje de error o un arreglo vacío, según sea necesario
+            return ['error' => 'Error en la consulta SQL'];
+        }
+    }
+    
+
 }
 ?>
